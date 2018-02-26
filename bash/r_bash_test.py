4b2634d7a236e0f7b15856a7de97741c68a0e001
Stefano Simonelli <stefano@dianomic.com>
2018-02-26 14:22:13 +0100

    wrk

--- bash/r_bash_test.py
+++ bash/r_bash_test.py
@@ -18,8 +18,9 @@ os.system(cmd)
 
 cmd = "cd {};".format(script_path)
 
-# cmd += "bash ./t1.sh"
-cmd += "bash /home/foglamp/wrk/task/FOGL-999/pi_delete.sh"
+cmd += "bash ./t1.sh"
+#cmd += "bash /home/foglamp/Development/FogLAMP/tests/system/tests/bash/pi_delete.bash"
+# cmd += "bash /home/foglamp/wrk/task/FOGL-999/pi_delete.sh"
 
 
 # ## Run #########################################################################################:
--- bash/t1.sh
+++ bash/t1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-file=/home/foglamp/Development/FogLAMP/tests/system/suites/end_to_end_PI/suite.cfg
+TMP_DIRECTORY=/test/2
 
-omf_type_id=`cat ${file} | grep -E '.*OMF_TYPE_ID.*=.*' | grep -o '[0-9]*'`
-new_omf_type_id=$((omf_type_id+1))
+echo TEST 1
 
-echo omf_type_id :${omf_type_id}:
-echo new_omf_type_id :${new_omf_type_id}:
+cat  << EOF
+curl -X GET -d '{"value": "file:///${TMP_DIRECTORY}"}'
+EOF
 
-sed -i -e s/${omf_type_id}/${new_omf_type_id}/ ${file}
+echo TEST 2
